8221dfa9252432fca7ddbc47959b25094d26fce4
Bashiru Yerima Sadat <124477655+bysadat@users.noreply.github.com>
2026-04-03 17:30:52 -0400

    Added the feautured Project sections to the Home Page

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -1,9 +1,30 @@
-const FeaturedProjects = () => {
+import type { Project } from '~/types';
+import ProjectCard from './ProjectCard';
+
+type FeaturedProjectsProps = {
+  projects: Project[];
+  count: number;
+};
+
+const FeaturedProjects = ({
+  projects,
+  count,
+}: {
+  projects: Project[];
+  count: number;
+}) => {
+  const featured = projects.filter((p) => p.featured).slice(0, count);
   return (
     <section>
-      <h2 className="text-2xl font-bold mb-4 bg-gradient-to-r from-sky-400 via-indigo-300 to-fuchsia-400 bg-clip-text text-transparent">
+      <h2 className="text-2xl text-center font-bold mb-4 bg-gradient-to-r from-sky-400 via-indigo-300 to-fuchsia-400 bg-clip-text text-transparent">
         Featured Projects
       </h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {featured.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </section>
   );
 };
